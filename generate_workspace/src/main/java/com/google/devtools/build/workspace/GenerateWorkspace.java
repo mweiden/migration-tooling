e0130a6a82c0b6e8321607bb5b698fa7c8739fee
Matt Weiden <matthewweiden@gmail.com>
2017-10-24 16:53:17 +0200

    Fix RemoteRepository ids

--- generate_workspace/src/main/java/com/google/devtools/build/workspace/GenerateWorkspace.java
+++ generate_workspace/src/main/java/com/google/devtools/build/workspace/GenerateWorkspace.java
@@ -88,7 +88,10 @@ public class GenerateWorkspace {
   ) throws IOException {
     List<RemoteRepository> repos = new ArrayList<RemoteRepository>();
     for (String repo : repositories) {
-      repos.add(new RemoteRepository.Builder("id", "default", repo).build());
+      repos.add(
+        new RemoteRepository.Builder("user defined repository", "default", repo)
+          .build()
+      );
     }
     DefaultModelResolver defaultModelResolver = new DefaultModelResolver(repos);
     for (String repo : repositories) {
